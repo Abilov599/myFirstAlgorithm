e92887e3fd0929edf8dbf3a4611ef45a7a1c0227
Abilov Jeyhun <abilovv599@gmail.com>
2022-10-30 16:17:52 +0400

    Task 1 is done

---
+++ myFirstAlgorithm.cs
@@ -0,0 +1,34 @@
+﻿namespace myFirstAlgorithm
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var num1 = 5;
+            var num2 = 10;
+            var num3 = 15;
+            var sum = 0;
+
+
+            if (num1 % 5 == 0)
+
+                sum += num1;
+
+
+            if (num2 % 10 == 0)
+
+                sum += num2;
+
+
+            if (num3 % 15 == 0)
+
+                sum += num3;
+
+            Console.WriteLine(sum);
+
+
+        }
+    }
+}
