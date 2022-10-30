53294ed0acf0ddb18974a8f5d17ace6ba6dbbc63
Abilov Jeyhun <abilovv599@gmail.com>
2022-10-30 23:13:04 +0400

    Task 3 is done

---
+++ Task3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        int a, b, c;
+
+        var sum = 0;
+        int abc = 278;
+        a = abc / 100;
+        b = abc / 10 % 10;
+        c = abc % 10;
+        sum = a + b + c;
+       Console.WriteLine(sum);
+
+        /*
+        
+        var sum = 0;
+            Console.Write("Enter the value of Number: "); 
+        var num = int.Parse(Console.ReadLine()!);
+        
+        sum = (num % 10) + ((num / 10) % 10) + ((num / 100) % 10);
+            Console.Write($"Summation is {sum}.");
+        */
+
+    }
+}
